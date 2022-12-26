ef5949b5719e7c94e6d084ea85d79cd12278153c
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-12-26 16:53:58 +0700

    Statistik Deskriptif

---
+++ kuliah/Tugas_akhir/tugas_akhir.R
@@ -0,0 +1,21 @@
+dataku <- read.csv('C:/Users/Thoriq/Documents/github/R/kuliah/Tugas_akhir/dataku.csv')
+dataku$Nilai_tengah
+dataku$Control_group_frequency
+dataku$Experiment_group_frequency
+
+data_cgf <- c(48.5, 48.5, 48.5, 48.5, 48.5, 48.5, 48.5, 48.5, 48.5,
+          56.5, 56.5, 56.5, 56.5, 64.5, 64.5, 64.5, 64.5, 64.5,
+          64.5, 64.5, 64.5, 64.5, 64.5, 72.5, 72.5, 72.5, 72.5,
+          72.5, 80.5, 80.5, 80.5, 80.5, 80.5, 88.5, 88.5, 88.5)
+summary(data_cgf)
+var(data_cgf)
+
+data_egf <- c(56.5, 56.5, 64.5, 64.5, 64.5, 64.5, 64.5, 64.5, 64.5,
+              72.5, 72.5, 72.5, 72.5, 72.5, 72.5, 80.5, 80.5, 80.5,
+              80.5, 80.5, 80.5, 80.5, 80.5, 80.5, 80.5, 80.5, 80.5,
+              80.5, 80.5, 80.5, 80.5, 88.5, 88.5, 88.5, 88.5, 88.5,
+              96.5, 96.5)
+
+library(nortest)
+lillie.test(data_cgf)
+lillie.test(data_egf)
